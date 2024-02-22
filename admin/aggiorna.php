a7e864cc2e991f501a7e99ba054a47b10fb6d34f
wollces <matteo.mungari@gmail.com>
2024-02-22 17:56:08 +0100

    aggiorna pratiche upload

--- admin/aggiorna.php
+++ admin/aggiorna.php
@@ -1,8 +1,9 @@
 <?php
 
 include_once '../inc/db.config.php';
-
+include_once '../upload_doc.php';
 session_start();
+
 if($_SESSION['login'] == false)
 {
     header('location: index.php');
@@ -13,17 +14,31 @@ $id = $_POST['id'];
 $corso = $_POST['aggiorna-corso'];
 $nome_utente = $_POST['aggiorna-nome-utente'];
 $nome_responsabile = $_POST['aggiorna-nome-responsabile'];
-$documenti = $_POST['aggiorna-documenti'];
 $stato_pratica = $_POST['aggiorna-stato'];
 
-$sql = "UPDATE pratiche SET corso=?, nome_utente=?, nome_responsabile=?, documenti=?, stato_pratica=?  WHERE id_pratica=?";
+if(!empty($_FILES['documenti']['name']))
+{
+    $documenti = $fileDestinazioneDatabase;
+    $sql = "UPDATE pratiche SET corso=?, nome_utente=?, nome_responsabile=?, documenti=?, stato_pratica=?  WHERE id_pratica=?";
 
-$stmt = $conn -> prepare( $sql );
-if ($stmt === FALSE) {
-    die('Error preparing statement: ' . $conn->error);
+    $stmt = $conn -> prepare( $sql );
+    if ($stmt === FALSE) {
+        die('Error preparing statement: ' . $conn->error);
+    }
+
+    $stmt -> bind_param('ssssii', $corso, $nome_utente, $nome_responsabile, $documenti, $stato_pratica, $id);
 }
+else 
+{
+    $sql = "UPDATE pratiche SET corso=?, nome_utente=?, nome_responsabile=?, stato_pratica=?  WHERE id_pratica=?";
 
-$stmt -> bind_param('ssssii', $corso, $nome_utente, $nome_responsabile, $documenti, $stato_pratica, $id);
+    $stmt = $conn -> prepare( $sql );
+    if ($stmt === FALSE) {
+        die('Error preparing statement: ' . $conn->error);
+    }
+
+    $stmt -> bind_param('sssii', $corso, $nome_utente, $nome_responsabile, $stato_pratica, $id);
+}
 
 if ( $stmt -> execute() === FALSE ) {
     die('non è possibile aggiornare i dati' . $stmt->error);
